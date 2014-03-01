276719d49a787b3774e6b4490ae461debdc76ed8
Agam Dua <agamdua@gmail.com>
2014-03-01 18:16:09 +0530

    Added: UI-bootstrap & typeahead

---
+++ app/scripts/controllers/registration.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('formDummyApp')
+.controller('RegistrationCtrl', function ($scope, $http) {
+	$scope.getLocation = function(val) {
+		return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+			params: {
+				address: val,
+				sensor: false
+			}
+		}).then(function(res){
+			var addresses = [];
+			angular.forEach(res.data.results, function(item){
+				addresses.push(item.formatted_address);
+			});
+			return addresses;
+		});
+	};
+});
